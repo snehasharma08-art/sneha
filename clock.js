6619bb22985ba00a33d882f4d3f5de873c5cb1a1
Sneha Sharma <snehasharmawos08@gmail.com>
2025-11-08 23:53:18 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,21 @@
+function updateClock() {
+  const clock = document.getElementById("clock");
+  const now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  //Add leading zeros
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  clock.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+//Update every second
+setInterval(updateClock, 1000);
+
+//Initial call to avoid delay
+updateClock();
